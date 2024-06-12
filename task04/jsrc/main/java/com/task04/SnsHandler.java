13ac86b69e4df536158e75c3be40555b66ef6327
Balint_Weisz <hcmf-wice@gmail.com>
2024-06-12 19:19:55 +0200

    task04

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -3,7 +3,7 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.DeploymentRuntime;
@@ -14,21 +14,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-		lambdaName = "sqs_handler",
-		roleName = "sqs_handler-role",
+		lambdaName = "sns_handler",
+		roleName = "sns_handler-role",
 		runtime = DeploymentRuntime.JAVA11,
 		isPublishVersion = false,
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @DependsOn(
-		name = "async_queue",
-		resourceType = ResourceType.SQS_QUEUE
+		name = "lambda_topic",
+		resourceType = ResourceType.SNS_TOPIC
 )
-@SqsTriggerEventSource(
-		targetQueue = "async_queue",
-		batchSize = 100
+@SnsEventSource(
+		targetTopic = "lambda_topic"
 )
-public class SqsHandler implements RequestHandler<Object, String> {
+public class SnsHandler implements RequestHandler<Object, String> {
 
 	public String handleRequest(Object request, Context context) {
 		LambdaLogger logger = context.getLogger();
